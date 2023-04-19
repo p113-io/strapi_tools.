985f70eca7d7eaa752e13800b034ca09f4e51d70
TomBombadilom <tom@croll.fr>
2023-04-19 15:43:33 -0300

    made old and new version prompt optional when there is no parameters or that new version dont match x.x.x

--- upgrade.sh
+++ upgrade.sh
@@ -31,41 +31,55 @@ nvm use $NODE_VERSION
 # update yarn and pm2
 npm install --location=global yarn pm2@latest
 
-# Définissez la chaîne de recherche par défaut
-# Récupération de la version à remplacer à partir du premier paramètre
-new_version=$1
-# Vérification si la variable est vide
-if [ -z "$new_version" ]; then
-  # Si la variable est vide, on définit une valeur par défaut
- new_version="4.9.2"
-fi
+# Définissez la chaîne de recherche par défaut old_version
 old_version=$(jq -r '.dependencies."@strapi/strapi"' package.json)
 echo "old_version found" $old_version
 search_string="$old_version"
 
-# Définissez la nouvelle chaîne de remplacement par défaut
-replace_string=$new_version
-read -p "Do you want to update the server (yes/no)? " choice
-
-if [ "$choice" == "yes" ]; then
-  # Demandez la chaîne de recherche, utilisez la valeur par défaut si aucune entrée n'est fournie
-  read -p "Enter the search string (default: $search_string): " input
-  search_string=${input:-$search_string}
-
-  # Demandez la nouvelle chaîne de remplacement, utilisez la valeur par défaut si aucune entrée n'est fournie
-  read -p "Enter the replace string (default: $replace_string): " input
-  replace_string=${input:-$replace_string}
-
-  # Exécutez le remplacement
-  sed -i "s/${search_string}/${replace_string}/g" package.json
-   
-  echo "package.json updated"
-
-  # rebuild strapi
-  ./build.sh
-
-  # restart server
-  ./start.sh
+# Récupération de la version à remplacer à partir du premier paramètre
+new_version=$1
+if [[ $new_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; 
+then
+  echo "La version $new_version est au bon format (x.x.x)."
 else
-  echo "The server will not be updated."
+  echo "La version $new_version n'est pas au bon format (x.x.x)."
 fi
+# Vérification si la variable est vide
+if [ -z "$new_version" ]; 
+then
+  # Si la variable est vide, on définit une valeur par défaut
+ new_version="4.9.2"
+  # Définissez la nouvelle chaîne de remplacement par défaut
+ replace_string=$new_version
+ read -p "Do you want to update the server (yes/no)? " choice
+
+  if [ "$choice" == "yes" ]; then
+    # Demandez la chaîne de recherche, utilisez la valeur par défaut si aucune entrée n'est fournie
+    read -p "Enter the search string (default: $search_string): " input
+    search_string=${input:-$search_string}
+
+    # Demandez la nouvelle chaîne de remplacement, utilisez la valeur par défaut si aucune entrée n'est fournie
+    read -p "Enter the replace string (default: $replace_string): " input
+    replace_string=${input:-$replace_string}
+
+    # Exécutez le remplacement
+    sed -i "s/${search_string}/${replace_string}/g" package.json
+  else
+    echo "The server will not be updated."
+  fi
+else
+  # pas besoin de prompts on peut l'utiliser pour upgrader le strapi
+  replace_string=$new_version
+   # Exécutez le remplacement
+  sed -i "s/${search_string}/${replace_string}/g" package.json   
+fi
+echo "package.json updated"
+
+# rebuild strapi
+./build.sh
+
+# restart server
+./start.sh
+
+
+
